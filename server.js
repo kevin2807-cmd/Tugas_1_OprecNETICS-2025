82adbdb80e3feb2a6535cbfc67440e99895e77d7
kevin2807-cmd <kl905577@gmail.com>
2025-04-05 11:47:28 +0700

    bisalah

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require("express");
 const app = express();
 const startTime = Date.now();
 
+let uptime = process.uptime();
+
 app.get("/health", (req, res) => {
   res.json({
     nama: "Kevin Leonard Berutu",
     NRP: "5025231089",
-    status: "Running",
+    status: "UP",
     timeStamp: new Date().toISOString(),
-    uptime: `${Math.floor((Date.now() - startTime) / 1000)}s`,
+    uptime: `${uptime}s`,
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () =>
   console.log(`Server running on: http://localhost:${port}`)
 );
